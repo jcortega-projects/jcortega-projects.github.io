af285209619b19c6416e935a1f189a77e0453933
Juan Ortega <jcortega@andrew.cmu.edu>
2026-02-18 18:15:01 -0500

    v2.6 bugs fixed - projects content is missing

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,5 +1,23 @@
 (function () {
   var STORAGE_KEY = "portfolio-theme";
+  function getAssetPrefix() {
+    var scriptTag = document.currentScript;
+    var scriptSrc = scriptTag ? scriptTag.getAttribute("src") || "" : "";
+    if (scriptSrc.indexOf("../../assets/scripts/theme.js") === 0) {
+      return "../../";
+    }
+    if (scriptSrc.indexOf("assets/scripts/theme.js") === 0) {
+      return "";
+    }
+    return window.location.pathname.indexOf("/projects/") !== -1 ? "../../" : "";
+  }
+  var assetPrefix = getAssetPrefix();
+  var ICON_PATHS = {
+    githubLight: assetPrefix + "assets/images/icons/github_logo_dark.png",
+    githubDark: assetPrefix + "assets/images/icons/github_logo_white.png",
+    toggleDark: assetPrefix + "assets/images/icons/night-mode.png",
+    toggleLight: assetPrefix + "assets/images/icons/day-mode.png"
+  };
   var root = document.documentElement;
   var button = document.getElementById("theme-toggle");
   var year = document.getElementById("year");
@@ -16,8 +34,8 @@
     root.setAttribute("data-theme", theme);
     var githubIcons = document.querySelectorAll(".github-icon");
     githubIcons.forEach(function (icon) {
-      var lightIcon = icon.getAttribute("data-light-icon") || "github_logo_dark.png";
-      var darkIcon = icon.getAttribute("data-dark-icon") || "github_logo_white.png";
+      var lightIcon = icon.getAttribute("data-light-icon") || icon.getAttribute("src") || ICON_PATHS.githubLight;
+      var darkIcon = icon.getAttribute("data-dark-icon") || icon.getAttribute("src") || ICON_PATHS.githubDark;
       icon.src = theme === "dark" ? darkIcon : lightIcon;
     });
 
@@ -25,13 +43,14 @@
       var icon = button.querySelector("img");
       if (!icon) {
         icon = document.createElement("img");
-        icon.setAttribute("data-dark-icon", button.getAttribute("data-dark-icon") || "night-mode.png");
-        icon.setAttribute("data-light-icon", button.getAttribute("data-light-icon") || "day-mode.png");
+        icon.setAttribute("data-dark-icon", button.getAttribute("data-dark-icon") || ICON_PATHS.toggleDark);
+        icon.setAttribute("data-light-icon", button.getAttribute("data-light-icon") || ICON_PATHS.toggleLight);
         button.textContent = "";
         button.appendChild(icon);
       }
-      var darkIcon = icon.getAttribute("data-dark-icon") || "night-mode.png";
-      var lightIcon = icon.getAttribute("data-light-icon") || "day-mode.png";
+      icon.classList.add("nav-icon");
+      var darkIcon = icon.getAttribute("data-dark-icon") || ICON_PATHS.toggleDark;
+      var lightIcon = icon.getAttribute("data-light-icon") || ICON_PATHS.toggleLight;
       var nextIsDark = theme !== "dark";
       icon.src = nextIsDark ? darkIcon : lightIcon;
       icon.alt = nextIsDark ? "Dark mode" : "Light mode";
